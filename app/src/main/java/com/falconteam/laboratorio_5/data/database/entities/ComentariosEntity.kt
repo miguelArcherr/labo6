acb3a54a94ef331a151ffd09a0df205581fccc3a
miguelarcherr <00199922@uca.edu.sv>
2024-06-14 13:50:09 -0600

    reinicio

--- app/src/main/java/com/falconteam/laboratorio_5/data/database/entities/ComentariosEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.falconteam.laboratorio_5.data.database.entities
-
-data class Comentarios(
-    val id: String,
-    val postId: String,
-    val author: String,
-    val comment: String
-)
